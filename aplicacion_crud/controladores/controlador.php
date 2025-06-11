a1cd69c9be7c52da8198aa8122fa4e3ee27708d0
luisQS13 <LUISVALENTINOQ.S@GMAIL.COM>
2025-06-11 09:09:33 -0500

    initial commit

---
+++ aplicacion_crud/controladores/controlador.php
@@ -0,0 +1,13 @@
+<?php
+// crearemos una clase
+class ControladorPaginas{
+
+    public function inicio(){
+        include_once("vistas/paginas/inicio.php");
+    }
+
+    public function error(){
+        include_once("vistas/paginas/error.php");
+    }
+}
+?>
